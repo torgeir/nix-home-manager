0eea2faec90dbc375e4c95de52e3fcbc883edd2a
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2024-11-17 11:11:11 +0100

    t-sway.command

--- modules/sway.nix
+++ modules/sway.nix
@@ -26,6 +26,12 @@ in {
     default = "i3status-rs ~/.config/i3status-rust/config.toml";
   };
 
+  options.programs.t-sway.command = lib.mkOption {
+    type = lib.types.nullOr lib.types.str;
+    example = "";
+    default = "";
+  };
+
   config = lib.mkIf cfg.enable {
 
     home.packages = with pkgs; [
@@ -131,6 +137,7 @@ in {
             names = [ "pango:IosevkaTerm Nerd Font" "FontAwesome" ];
             size = 11.0;
           };
+          command = cfg.command;
           statusCommand = cfg.statusCommand;
 
           #colors = {
