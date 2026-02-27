b34f355e77dd86197cdac17bc858838141bc868d
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2026-02-27 13:59:51 +0100

    graphviz

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,40 +1,52 @@
-{ inputs, config, lib, pkgs, ... }:
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.programs.t-doomemacs;
-  emacs = if pkgs.stdenv.isDarwin then
-    (pkgs.emacs30.overrideAttrs (old: {
-      # inspiration https://github.com/noctuid/dotfiles/blob/30f615d0a8aed54cb21c9a55fa9c50e5a6298e80/nix/overlays/emacs.nix
-      patches = (old.patches or [ ]) ++ [
-        # fix os window role so that yabai can pick up emacs
-        (pkgs.fetchpatch {
-          url =
-            "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
-          sha256 = "+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
-        })
-        (pkgs.fetchpatch {
-          url =
-            "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/round-undecorated-frame.patch";
-          sha256 = "uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
-        })
-      ];
-    })).override {
-      # TODO still not working on macos
-      withNativeCompilation = pkgs.stdenv.isLinux;
-    }
-  else
-    pkgs.emacs30-pgtk;
+  emacs =
+    if pkgs.stdenv.isDarwin then
+      (pkgs.emacs30.overrideAttrs (old: {
+        # inspiration https://github.com/noctuid/dotfiles/blob/30f615d0a8aed54cb21c9a55fa9c50e5a6298e80/nix/overlays/emacs.nix
+        patches = (old.patches or [ ]) ++ [
+          # fix os window role so that yabai can pick up emacs
+          (pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+            sha256 = "+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+          })
+          (pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/round-undecorated-frame.patch";
+            sha256 = "uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
+          })
+        ];
+      })).override
+        {
+          # TODO still not working on macos
+          withNativeCompilation = pkgs.stdenv.isLinux;
+        }
+    else
+      pkgs.emacs30-pgtk;
   treesit = (pkgs.emacsPackagesFor emacs).treesit-grammars.with-all-grammars;
-in {
+in
+{
 
-  options.programs.t-doomemacs.enable =
-    lib.mkEnableOption "Enable doom emacs configuration.";
+  options.programs.t-doomemacs.enable = lib.mkEnableOption "Enable doom emacs configuration.";
 
   config = lib.mkIf cfg.enable {
 
     programs.emacs = {
       enable = true;
-      package = ((pkgs.emacsPackagesFor emacs).emacsWithPackages
-        (epkgs: [ epkgs.vterm epkgs.mu4e epkgs.pdf-tools treesit ]));
+      package = (
+        (pkgs.emacsPackagesFor emacs).emacsWithPackages (epkgs: [
+          epkgs.vterm
+          epkgs.mu4e
+          epkgs.pdf-tools
+          treesit
+        ])
+      );
     };
 
     xdg.enable = true;
@@ -51,6 +63,9 @@ in {
         # emacs lsp
         nil # nix lsp https://github.com/oxalica/nil
         nixfmt
+
+        # dot
+        graphviz
 
         # emacs deps
         nodejs_20
@@ -76,8 +91,7 @@ in {
         # where doom writes cache etc
         DOOMLOCALDIR = "${config.xdg.configHome}/doom-local";
         # where doom writes one more file
-        DOOMPROFILELOADFILE =
-          "${config.xdg.configHome}/doom-local/cache/profile-load.el";
+        DOOMPROFILELOADFILE = "${config.xdg.configHome}/doom-local/cache/profile-load.el";
 
         EDITOR = "emacsclient";
         VISUAL = "emacsclient";
@@ -87,8 +101,7 @@ in {
       # tree-sitter subdirectory of the directory specified by user-emacs-directory
       "doom-local/cache/tree-sitter".source = "${treesit}/lib";
       # git clone git@github.com:torgeir/.emacs.d.git ~/.doom.d
-      "doom.d".source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/.doom.d";
+      "doom.d".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.doom.d";
       "emacs" = {
         source = inputs.doomemacs;
         # rev bumps will make doom sync run
