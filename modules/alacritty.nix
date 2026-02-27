eb1f1fd1fbaec1917572c60ceaaa77965fc1cf38
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2026-02-27 08:53:57 +0100

    alacritty ueberzugpp linux

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -1,16 +1,22 @@
-{ dotfiles, config, lib, pkgs, ... }:
+{
+  dotfiles,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
-let cfg = config.programs.t-terminal.alacritty;
-in {
+let
+  cfg = config.programs.t-terminal.alacritty;
+in
+{
 
-  options.programs.t-terminal.alacritty.enable =
-    lib.mkEnableOption "Enable alacritty configuration.";
+  options.programs.t-terminal.alacritty.enable = lib.mkEnableOption "Enable alacritty configuration.";
 
-  options.programs.t-terminal.alacritty.package =
-    lib.mkPackageOption pkgs "alacritty" {
-      default = "alacritty";
-      example = "pkgs.unstable.alacritty";
-    };
+  options.programs.t-terminal.alacritty.package = lib.mkPackageOption pkgs "alacritty" {
+    default = "alacritty";
+    example = "pkgs.unstable.alacritty";
+  };
 
   config = lib.mkIf cfg.enable {
 
@@ -19,50 +25,50 @@ in {
       package = cfg.package;
     };
 
-    home.file = if pkgs.stdenv.isDarwin then {
-      ".config/alacritty/main.toml".source = dotfiles
-        + "/config/alacritty/main.toml";
-      ".config/alacritty/alacritty-dark.toml".source = dotfiles
-        + "/config/alacritty/alacritty-dark.toml";
-      ".config/alacritty/alacritty-light.toml".source = dotfiles
-        + "/config/alacritty/alacritty-light.toml";
-      ".config/alacritty/catppuccin-mocha.toml".source = dotfiles
-        + "/config/alacritty/catppuccin-mocha.toml";
-      ".config/alacritty/catppuccin-latte.toml".source = dotfiles
-        + "/config/alacritty/catppuccin-latte.toml";
+    home.file =
+      if pkgs.stdenv.isDarwin then
+        {
+          ".config/alacritty/main.toml".source = dotfiles + "/config/alacritty/main.toml";
+          ".config/alacritty/alacritty-dark.toml".source = dotfiles + "/config/alacritty/alacritty-dark.toml";
+          ".config/alacritty/alacritty-light.toml".source =
+            dotfiles + "/config/alacritty/alacritty-light.toml";
+          ".config/alacritty/catppuccin-mocha.toml".source =
+            dotfiles + "/config/alacritty/catppuccin-mocha.toml";
+          ".config/alacritty/catppuccin-latte.toml".source =
+            dotfiles + "/config/alacritty/catppuccin-latte.toml";
 
-      # hack to switch theme on macos only
-      ".config/alacritty/alacritty-toggle-appearance" = {
-        text = ''
-          #!/usr/bin/env bash
-          cd ${config.xdg.configHome}/alacritty/
-          darkmode=$(osascript -e 'tell application "System Events" to get dark mode of appearance preferences')
-          if [ "true" = "$darkmode" ]; then
-            cp -f alacritty-dark.toml alacritty.toml
-          else
-            cp -f alacritty-light.toml alacritty.toml
-          fi
-        '';
-        executable = true;
-      };
+          # hack to switch theme on macos only
+          ".config/alacritty/alacritty-toggle-appearance" = {
+            text = ''
+              #!/usr/bin/env bash
+              cd ${config.xdg.configHome}/alacritty/
+              darkmode=$(osascript -e 'tell application "System Events" to get dark mode of appearance preferences')
+              if [ "true" = "$darkmode" ]; then
+                cp -f alacritty-dark.toml alacritty.toml
+              else
+                cp -f alacritty-light.toml alacritty.toml
+              fi
+            '';
+            executable = true;
+          };
 
-      ".config/alacritty/alacritty-toggle-appearance".onChange = ''
-        #!/usr/bin/env bash
-        cd ${config.xdg.configHome}/alacritty/
-        if [ ! -f alacritty.toml ]; then
-          cp -f alacritty-dark.toml alacritty.toml
-        fi
-        sudo chown torgeir ${config.xdg.configHome}/alacritty/alacritty-toggle-appearance
-        sudo chmod u+x ${config.xdg.configHome}/alacritty/alacritty-toggle-appearance
-      '';
-    } else {
-      ".config/alacritty/catppuccin-mocha.toml".source = dotfiles
-        + "/config/alacritty/catppuccin-mocha.toml";
-      ".config/alacritty/alacritty.toml".source = dotfiles
-        + "/config/alacritty/alacritty.toml";
-      ".config/alacritty/main.toml".source = dotfiles
-        + "/config/alacritty/main.toml";
-    };
+          ".config/alacritty/alacritty-toggle-appearance".onChange = ''
+            #!/usr/bin/env bash
+            cd ${config.xdg.configHome}/alacritty/
+            if [ ! -f alacritty.toml ]; then
+              cp -f alacritty-dark.toml alacritty.toml
+            fi
+            sudo chown torgeir ${config.xdg.configHome}/alacritty/alacritty-toggle-appearance
+            sudo chmod u+x ${config.xdg.configHome}/alacritty/alacritty-toggle-appearance
+          '';
+        }
+      else
+        {
+          ".config/alacritty/catppuccin-mocha.toml".source =
+            dotfiles + "/config/alacritty/catppuccin-mocha.toml";
+          ".config/alacritty/alacritty.toml".source = dotfiles + "/config/alacritty/alacritty.toml";
+          ".config/alacritty/main.toml".source = dotfiles + "/config/alacritty/main.toml";
+        };
 
     home.packages = [
       (pkgs.writeShellScriptBin "alacritty-open" ''
@@ -106,7 +112,8 @@ in {
             ;;
         esac
       '')
-    ];
+    ]
+    ++ (if pkgs.stdenv.isDarwin then [ ] else [ pkgs.ueberzugpp ]);
   };
 
 }
