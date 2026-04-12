ec5e0b0abbbbdcccfa579edf5ece4d0bedb9ca58
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2026-04-12 08:37:02 +0200

    firefox remove sidebar header user chrome

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -67,7 +67,6 @@ in {
         # https://github.com/nix-community/home-manager/blob/master/modules/programs/firefox.nix
         userChrome = ''
           #TabsToolbar    { visibility: collapse !important; }
-          #sidebar-header { visibility: collapse !important; }
           #urlbar-zoom-button { display: block; }
         '';
         #https://github.com/montchr/dotfield/blob/78de8ff316ccb2d34fd98cd9bfd3bfb5ad775b0e/home/profiles/firefox/search/default.nix
